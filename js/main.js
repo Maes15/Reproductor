1d981601d59286fddc97a2a43afa2fe9603879d0
Maes15 <maestreca97@gmail.com>
2018-11-21 12:44:32 +0100

    Muestra las caratulas del id que busca

--- js/main.js
+++ js/main.js
@@ -1,33 +1,16 @@
-function miOnDrag (event) {
-
-    console.log('Arrastrando elemento', event.target)
+function miOnStart(event) {
+    event.dataTransfer.setData('id', event.target.id);
+    console.log('Drag Start', event.target.id,  event.dataTransfer.getData('id'));
+    // return event.target.id;
 }
-
-function miOnStart(e) {
-
-    e.dataTransfer.setData('id', e.target.id);
-
-    console.log('Drag Start', e.target.id, ' - ', e.dataTransfer.getData('id'))
+function miOnOver(event) {
+    event.preventDefault();
+    console.log('Drag Over');
 }
-function miOnEnter(e) {
-    console.log('Drag Enter')
+function miOnDrop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData('id');
+    console.log(data);
+    event.target.appendChild(document.getElementById(data));
+    return data;
 }
-function miOnLeave(e) {
-    console.log('Drag Leave')
-}
-function miOnOver(e) {
-    e.preventDefault();
-        console.log('Drag Over')
-}
-
-function miOnEnd(e) {
-    console.log('Drag DragEnd')
-}
-function miOnDrop(ev) {
-    ev.preventDefault();// prevenir el comportamiento por defecto
-    var data = ev.dataTransfer.getData('id'); // guarda los datos de id
-    ev.target.appendChild(document.getElementById(data))   //
-
-    console.log('Drag Drop', data)
-}
-
